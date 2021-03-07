11c65659fda3aa2008afce7a872baf397e2a6e16
Nick Bird <bird.nick.95@gmail.com>
2021-03-07 12:53:36 -0700

    added request mapping for updateFilm to controller

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.dao.FilmDAO;
 import com.skilldistillery.film.entities.Film;
@@ -19,7 +20,7 @@ public class FilmController {
 	private FilmDAO filmDAO;
 	
 	@RequestMapping(path= {"/", "home.do"})
-	public String home(Model model) {
+	public String home() {
 		// TODO - add list of all films to model?
 		
 		
@@ -27,4 +28,16 @@ public class FilmController {
 		return "WEB-INF/views/index.jsp";
 	}
 
+@RequestMapping(path="updatefilm.do")
+public ModelAndView updateFilm(int filmID, String title, String description, Integer releaseYear, Integer rentalDuration,
+		Double rentalRate, Integer length, Double replacementCost, String rating, String specialFeatures) {
+	ModelAndView mv = new ModelAndView();
+	mv.addObject("updatedFilm",filmDAO.updateFilm(filmID, title, description, releaseYear, rentalDuration, 
+			rentalRate, length, replacementCost, rating, specialFeatures));
+	mv.setViewName("WEB-INF/views/UpdateFilm.jsp");
+	
+	return mv;
+}
+	
+	
 }
